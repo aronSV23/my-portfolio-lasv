acb32877787770783935097eb10fe1a67c92d3a5
aron <aronsotomarino@gmail.com>
2024-04-17 15:10:23 -0500

    first-commite

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1>Mi portfolio</h1>
+        <nav>
+          <u>
+            <il>Home</il>
+            <il>about</il>
+            <il>education</il>
+            <il>projects</il>
+            <il>experience</il>
+          </u>
+        </nav>
+      </header>
+    </>
+  )
+}
+
+export default App
